01a843f8b15f989426a4650088ac406262c6bab4
Abdul BOURAe <abdul.boura@gmail.com>
2024-07-31 17:17:35 +0200

    model trainer done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,7 +6,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
-
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 '''
 This class is responsible for reading the data from the given path and splitting it into train and test data
 It saves the train and test data in the artifacts folder
@@ -60,3 +61,6 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
